3023106f381cd049b92d4ee748f120a0711846fa
Dannydcodewriter <95192213+Dannydcodewriter@users.noreply.github.com>
2022-07-11 12:29:36 +0100

    added first tutorial file

---
+++ tutorial.py
@@ -0,0 +1,22 @@
+#from itertools import count
+l = [1,2,3,4,5,]
+print(l[1:4])
+
+
+
+
+
+
+
+
+'''game = [[0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0],]
+
+print("   a  b  c")
+
+for count, row in enumerate (game):
+    print(count, row)'''  
+
+    
+    
