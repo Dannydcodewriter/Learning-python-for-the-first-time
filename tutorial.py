d2f8bf39fa52b5ce3d1237bd26f05164fd83bcf7
Dannydcodewriter <95192213+Dannydcodewriter@users.noreply.github.com>
2022-07-11 17:53:02 +0100

    added files

--- tutorial.py
+++ tutorial.py
@@ -1,22 +1,15 @@
-#from itertools import count
-l = [1,2,3,4,5,]
-print(l[1:4])
+# from itertools import count
 
-
-
-
-
-
+# understanding indexes and slices
+l = [1, 2, 3, 4, 5, ]
+print(l[1])
 
 
 '''game = [[0, 0, 0],
         [0, 0, 0],
-        [0, 0, 0],]
+        [0, 0, 0], ]
 
 print("   a  b  c")
 
-for count, row in enumerate (game):
-    print(count, row)'''  
-
-    
-    
+for count, row in enumerate(game):
+    print(count, row)'''
